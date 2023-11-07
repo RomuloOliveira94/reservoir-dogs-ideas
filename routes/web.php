379bb74de8e63b3fc4b72464f81f4b6ae29e57be
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-07 19:53:33 -0300

    #create route for follows

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\IdeaController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -40,6 +40,10 @@ Route::resource('/users', UserController::class)->only(['show', 'edit', 'update'
 
 Route::get('/profile', [UserController::class,'profile'])->middleware('auth')->name('profile');
 
+Route::post('users/{user}/follow', [FollowerController::class,'follow'])->middleware('auth')->name('users.follow');
+
+Route::post('users/{user}/unfollow', [FollowerController::class,'unfollow'])->middleware('auth')->name('users.unfollow');
+
 Route::get('/terms', function () {
     return view('terms');
-});
+})->name('terms');
