0a0c7291be271ca4cb3780ebb87ee4cc70e6de08
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-07 19:54:29 -0300

    #implements terms page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,12 +1,20 @@
 @extends('layout.layout')
 @section('content')
-<div>
-    <h1>termos</h1>
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left_side_bar')
+        </div>
+        <div class="col-6">
+            <h1>termos</h1>
     <div>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores dolorum officiis dolore dolor incidunt explicabo, quasi ab laborum eveniet dicta quibusdam veritatis nostrum quae distinctio quo, itaque provident? Porro, explicabo!
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum aspernatur adipisci rerum sapiente tempore impedit, atque veniam quisquam sed a dolores est blanditiis fugiat molestias iusto vel fuga quia natus.
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum accusantium velit ut dignissimos repellat quam at. Ut totam, obcaecati ipsam culpa libero maxime quam neque dignissimos, exercitationem ducimus at vel!Sint facere dolorem deleniti. Labore corporis velit architecto minima ullam repellendus eveniet molestiae rem dolore aperiam consequatur repudiandae eligendi voluptate eius illum tempora numquam ut, aspernatur in accusamus quos vitae!
     </div>
-</div>
+        </div>
+        <div class="col-3">
+            @include('shared.search')
+            @include('shared.follow_box')
+        </div>
+    </div>
 @endsection
-
