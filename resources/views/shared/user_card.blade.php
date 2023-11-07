1ce5eb6153977ab5d9a739b5385da6ba08e40e8b
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-07 19:54:08 -0300

    #fix follow button

--- resources/views/shared/user_card.blade.php
+++ resources/views/shared/user_card.blade.php
@@ -2,12 +2,12 @@
     <div class="px-3 pt-4 pb-2">
         <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center">
-                <img width="150px" height="150px" class="me-3 avatar-sm rounded-circle"
-                    src="{{$user->getImageURL()}}" alt="{{$user->name}} avatar">
+                <img width="150px" height="150px" class="me-3 avatar-sm rounded-circle" src="{{ $user->getImageURL() }}"
+                    alt="{{ $user->name }} avatar">
                 <div>
                     <h3 class="card-title mb-0"><a href="#"> {{ $user->name }}
                         </a></h3>
-                    <span class="fs-6 text-muted">@mario</span>
+                    <span class="fs-6 text-muted">{{ '@' . $user->name }}</span>
                 </div>
             </div>
             <div>
@@ -27,7 +27,7 @@
         <div class="px-2 mt-4">
             <h5 class="fs-5"> Bio : </h5>
             <p class="fs-6 fw-light">
-                {{$user->bio}}
+                {{ $user->bio }}
             </p>
             <div class="d-flex justify-content-start">
                 <a href="#" class="fw-light nav-link fs-6 me-3"> <span class="fas fa-user me-1">
@@ -40,7 +40,17 @@
             @auth
                 @if (Auth::id() !== $user->id)
                     <div class="mt-3">
-                        <button class="btn btn-primary btn-sm"> Follow </button>
+                        @if (Auth::user()->follows($user))
+                            <form action="{{ route('users.unfollow', $user->id) }}" method="post">
+                                @csrf
+                                <button class="btn btn-danger btn-sm mb-2"> Unfollow </button>
+                            </form>
+                        @else
+                            <form action="{{ route('users.follow', $user->id) }}" method="post">
+                                @csrf
+                                <button class="btn btn-primary btn-sm mb-2"> Follow </button>
+                            </form>
+                        @endif
                     </div>
                 @endif
             @endauth
