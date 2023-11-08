db19757c0c944769a976f0ae1ef492a06f213e38
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-07 21:52:50 -0300

    #implement functions to like functionality

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -30,4 +30,8 @@ class Idea extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function likes(){
+        return $this->belongsToMany(User::class,'idea_like')->withTimestamps();
+    }
 }
