15bedebb60bb4f0e30509b8d64d34e3d28bb6b5b
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-08 10:04:04 -0300

    #fix nav links size

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark ticky-top bg-body-tertiary"
     data-bs-theme="dark">
     <div class="container">
-        <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1">
+        <a class="navbar-brand fw-bold fs-3" href="/"><span class="fas fa-ban me-1 fs-3">
             </span>{{ config('app.name') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,10 +11,10 @@
             <ul class="navbar-nav">
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link {{Route::is('login') ? 'active' : ''}}" aria-current="page" href="{{route('login')}}">Login</a>
+                        <a class="nav-link {{Route::is('login') ? 'active' : ''}} fs-5" aria-current="page" href="{{route('login')}}">Entra!</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{Route::is('register') ? 'active' : ''}}" href="{{route('register')}}">Register</a>
+                        <a class="nav-link {{Route::is('register') ? 'active' : ''}} fs-5" href="{{route('register')}}">Cadastra!</a>
                     </li>
                 @endguest
                 @auth
