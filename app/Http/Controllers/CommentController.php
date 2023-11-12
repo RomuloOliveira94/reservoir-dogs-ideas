46174efd2a829172bc72b59f3f1ad10370a54d58
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-12 13:52:04 -0300

    #implement comment form request

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCommentRequest;
 use App\Models\Comment;
 use App\Models\Idea;
 use Dotenv\Validator;
@@ -10,29 +11,20 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    public function store(Idea $idea){
-        $res_comment = request()->get("comment", []);
+    public function store(CreateCommentRequest $request, Idea $idea){
 
+        $validated = $request->validated();
 
-        request()->validate([
-            "comment.$idea->id" => ['required','string','max:240'],
-        ]);
+        $validated['user_id'] = auth()->id();
+        $validated['idea_id'] = $idea->id;
 
-        $comment = new Comment([
-            'idea_id'=> $idea->id,
-            'content'=> $res_comment[$idea->id],
-            'user_id'=> auth()->user()->id,
-        ]);
-
-        $comment->save();
+        Comment::create($validated);
 
         return redirect()->route('ideas.show', $idea->id)->with('success','Comment created successfully.');
     }
     public function destroy(Idea $idea, Comment $comment){
 
-        if(auth()->user()->id != $comment->user_id){
-            abort(404);
-        }
+        $this->authorize('delete', $comment);
 
         $comment->delete();
         return redirect()->route('ideas.show', $idea->id)->with('success','Comment deleted successfully.');
---
+++ resources/views/ideas/shared/comments_box.blade.php
@@ -0,0 +1,55 @@
+<div>
+    <form action="{{ route('ideas.comments.store', $idea->id) }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <textarea class="fs-6 form-control" rows="1" name="content" id="content"></textarea>
+            @error("content", $idea->id)
+                <span class="fs-5 ms-1 text-danger"> {{ $message }}</span>
+            @enderror
+        </div>
+        <div>
+            <button class="btn btn-primary btn-sm" type="submit"> Post Comment </button>
+        </div>
+    </form>
+
+    <hr>
+    @forelse ($idea->comments as $comment)
+        <div class="d-flex align-items-start mb-2">
+            <a href="{{ route('users.show', $comment->user->id) }}"><img width="50px" height="50px"
+                    class="me-2 avatar-sm rounded-circle" src="{{ $comment->user->getImageURL() }}"
+                    alt="{{ $comment->user->name }} avatar">
+            </a>
+            <div class="w-100">
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <h6 class=""><a
+                                href="{{ route('users.show', $comment->user->id) }}">{{ $comment->user->name }}</a>
+                        </h6>
+                        <p class="fs-6 mt-1 fw-light d-flex">
+                            {{ $comment->content }}
+                        </p>
+                    </div>
+                    <div class="d-flex flex-column inline-block">
+                        <small class="fs-6 fw-light text-muted"> {{ $comment->created_at->diffForHumans() }}</small>
+                        @auth
+                            @can('delete', $comment)
+                                <form class="d-flex justify-content-end"
+                                    action="{{ route('ideas.comments.destroy', [
+                                        'idea' => $idea->id,
+                                        'comment' => $comment->id,
+                                    ]) }}"
+                                    method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-sm btn-danger w-1">X</button>
+                                </form>
+                            @endcan
+                        @endauth
+                    </div>
+                </div>
+            </div>
+        </div>
+    @empty
+        <h5 class="text-center">No comments</h5>
+    @endforelse
+</div>
